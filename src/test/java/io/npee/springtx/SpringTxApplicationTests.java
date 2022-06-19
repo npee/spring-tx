b6ec71315d4e1d69a965ba5897cc2d3862cf4e96
npee <jmc4664@gmail.com>
2022-06-19 14:58:24 +0900

    Initial Commit

---
+++ src/test/java/io/npee/springtx/SpringTxApplicationTests.java
@@ -0,0 +1,13 @@
+package io.npee.springtx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
