ee16c73d42357ce7bd017b18da755ad4b04c8976
npee <jmc4664@gmail.com>
2022-06-23 23:35:09 +0900

    앱 초기화 시 트랜잭션 호출 테스트

---
+++ src/test/java/io/npee/springtx/apply/InitTxTest.java
@@ -0,0 +1,47 @@
+package io.npee.springtx.apply;
+
+import javax.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@SpringBootTest
+public class InitTxTest {
+
+    @Autowired
+    Hello hello;
+
+    @Test
+    void go() {
+        // @PostConstruct 이후에 AOP 적용됨
+    }
+
+    @TestConfiguration
+    static class InitTxTestConfig {
+
+        @Bean
+        Hello hello() {
+            return new Hello();
+        }
+    }
+
+    @Slf4j
+    static class Hello {
+
+        @PostConstruct
+        @Transactional
+        public void initV1() {
+            boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("Is tx of Hello class with @PostConstruct activated? {}", isActive);
+        }
+
+        public void initV2() {
+
+        }
+    }
+}
