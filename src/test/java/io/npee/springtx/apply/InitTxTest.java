6807c75b9c2004c6eea653e18bb4f14f70df914e
npee <jmc4664@gmail.com>
2022-06-24 07:20:25 +0900

    앱 초기화 이후 트랜잭션 호출 테스트

--- src/test/java/io/npee/springtx/apply/InitTxTest.java
+++ src/test/java/io/npee/springtx/apply/InitTxTest.java
@@ -4,9 +4,11 @@ import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
@@ -19,6 +21,7 @@ public class InitTxTest {
     @Test
     void go() {
         // @PostConstruct 이후에 AOP 적용됨
+        // @EventListener(ApplicationReadyEvent.class) 적용 시 앱이 로드된 이후에 적용됨
     }
 
     @TestConfiguration
@@ -40,8 +43,11 @@ public class InitTxTest {
             log.info("Is tx of Hello class with @PostConstruct activated? {}", isActive);
         }
 
+        @EventListener(ApplicationReadyEvent.class)
+        @Transactional
         public void initV2() {
-
+            boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("Is tx of Hello class with @EventListener  activated? {}", isActive);
         }
     }
 }
