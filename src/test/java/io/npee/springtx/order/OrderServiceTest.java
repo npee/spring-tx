4181af4bf7d9cb2f5ac3e98bb422ee25b59517bd
npee <jmc4664@gmail.com>
2022-06-26 21:39:21 +0900

    정상인 경우 테스트

---
+++ src/test/java/io/npee/springtx/order/OrderServiceTest.java
@@ -0,0 +1,35 @@
+package io.npee.springtx.order;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+class OrderServiceTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void complete() throws NotEnoughMoneyException {
+        //given
+        Order order = new Order();
+        order.setUsername("정상");
+
+        //when
+        orderService.order(order);
+
+        //then
+        Order findOrder = orderRepository.findById(order.getId()).get();
+        Assertions.assertThat(findOrder.getPayStatus()).isEqualTo("완료");
+    }
+
+}
