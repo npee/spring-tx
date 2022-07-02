58082d3610eb7f45df502d61f941feaa22b7cccd
npee <jmc4664@gmail.com>
2022-07-02 16:42:58 +0900

    회원, 로그저장용 entity, repository 생성

---
+++ src/main/java/io/npee/springtx/propagation/Log.java
@@ -0,0 +1,22 @@
+package io.npee.springtx.propagation;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Log {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String message;
+}
---
+++ src/main/java/io/npee/springtx/propagation/Member.java
@@ -0,0 +1,20 @@
+package io.npee.springtx.propagation;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+
+}
