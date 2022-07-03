53f970f220190a7d825561d49f6a4346c18f6c20
npee <jmc4664@gmail.com>
2022-07-03 18:12:25 +0900

    로그 수정 및 V1 테스트 작성

--- src/main/java/io/npee/springtx/propagation/MemberService.java
+++ src/main/java/io/npee/springtx/propagation/MemberService.java
@@ -40,7 +40,7 @@ public class MemberService {
             log.info("log 저장에 실패했습니다. logMessage {}", logMessage.getMessage());
             log.info("정상 흐름 반환");
         }
-        log.info("== logRepository 호출 시작 ==");
+        log.info("== logRepository 호출 종료 ==");
     }
 
 }
---
+++ src/test/java/io/npee/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,37 @@
+package io.npee.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    LogRepository logRepository;
+
+    /**
+     * memberService    @Transaction: OFF
+     * memberRepository @Transaction: ON
+     * logRepository    @Transaction: ON
+     */
+    @Test
+    void outer_tx_off_success() {
+        String username = "outerTxOff_success";
+
+        memberService.joinV1(username);
+
+        assertTrue(memberRepository.find(username).isPresent());
+    }
+}
